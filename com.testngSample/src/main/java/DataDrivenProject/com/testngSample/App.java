d371e5e128a4bead401f28e82cdef0828c6b23a8
DELL <DELL@DESKTOP-1CR4BJU>
2019-09-12 12:56:44 +0530

    first commit

---
+++ com.testngSample/src/main/java/DataDrivenProject/com/testngSample/App.java
@@ -0,0 +1,13 @@
+package DataDrivenProject.com.testngSample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
